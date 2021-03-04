2450b78506b203124624327fcba3383f9e499c6d
Michael Taggart <mtaggarthawk@gmail.com>
2021-03-03 20:13:44 -0700

    init

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import './App.css';
 
 const Header = props => {
- const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+
 
 
  return (
-  
+  <div>
+
+  </div>
 
  );
 }
